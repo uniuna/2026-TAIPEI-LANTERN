dae2abfda52f8bd42d86f1f9647feb14c0481366
una_1206 <qutimore@gmail.com>
2026-01-15 23:16:25 +0800

    新增霞海城隍廟與JINS的贊助商標誌，更新圖片資源以提升網站內容

--- footer.php
+++ footer.php
@@ -29,11 +29,13 @@
                 <div class="logo-box"><img src="images/logos/logo--Ci-Hui.png" alt="慈惠堂" loading="lazy" decoding="async"></div>
                 <div class="logo-box"><img src="images/logos/logo-Ciyou.png" alt="慈祐宮" loading="lazy" decoding="async"></div>
                 <div class="logo-box"><img src="images/logos/logo-LungShan.png" alt="龍山寺" loading="lazy" decoding="async"></div>
+                <div class="logo-box"><img src="images/logos/logo-Xiahai.png" alt="霞海城隍廟" loading="lazy" decoding="async"></div>
                 <div class="logo-box"><img src="images/logos/logo-Fubon.png" alt="富邦" loading="lazy" decoding="async"></div>
                 <div class="logo-box"><img src="images/logos/logo-FarEastern.png" alt="遠東" loading="lazy" decoding="async"></div>
                 <div class="logo-box"><img src="images/logos/logo-Cathay.png" alt="國泰" loading="lazy" decoding="async"></div>
+                <div class="logo-box"><img src="images/logos/logo-JINS.png" alt="JINS" loading="lazy" decoding="async"></div>
                 <div class="logo-box"><img src="images/logos/logo-ShinKong.png" alt="台新新光" loading="lazy" decoding="async"></div>
-                <div class="logo-box"><img src="images/logos/logo-TingHsin.png" alt="頂新" loading="lazy" decoding="async"></div>   
+                <div class="logo-box"><img src="images/logos/logo-TingHsin.png" alt="頂新" loading="lazy" decoding="async"></div>
                 <div class="logo-box"><img src="images/logos/logo-TWM.png" alt="台哥大" loading="lazy" decoding="async"></div>
             </div>
         </div>
