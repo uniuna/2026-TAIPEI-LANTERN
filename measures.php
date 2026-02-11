5a336f71e55b514b583d3448bb92546a940775ff
unachen <qutimore@gmail.com>
2026-02-11 17:01:12 +0800

    feat(measures): 新增交通管制措施及示意圖，更新相關內容

--- measures.php
+++ measures.php
@@ -14,13 +14,62 @@
         </div>
     </header>
 
-    <main class="container" style="padding-top: 150px;">
-        
-        <section>
+    <main class="container">
+        <h2 class="section-title reminder-intro">交通管制措施</h2>
+
+        <div class="traffic-control-section">
             
-            <h2 class="section-title reminder-intro">規劃中</h2>
-            
-        </section>
+            <div class="traffic-control-card">
+                <h3 class="traffic-control-title">西門展區交通管制</h3>
+                <ul class="traffic-list">
+                    <li>
+                        <strong>道路封閉管制：</strong><br>
+                        2月26日(四)開幕日及假日，自 16:00 至 22:00 封閉漢中街(成都路至內江街路段)雙向，及漢中街(內江街至長沙街路段)北向禁行車輛。
+                    </li>
+                    <li>
+                        <strong>計程車排班區取消：</strong><br>
+                        2月26日(四)開幕日，中華路西側臨近成都路口計程車排班區取消。
+                    </li>
+                    <li>
+                        <strong>車輛通行管制：</strong><br>
+                        2月25日(三)至3月15日(日)活動期間，平日 17:00-22:00、假日 16:00-22:00，管制酉陽街(含延平南路武昌至酉陽路段)、秀山街車輛通行（僅供飯店、停車場車輛通行）。
+                    </li>
+                </ul>
+
+                <h3 class="traffic-control-title" style="margin-top: 40px;">自行車與 YouBike 管制</h3>
+                <ul class="traffic-list">
+                    <li>
+                        <strong>YouBike 暫停服務：</strong><br>
+                        1. <strong>中山堂武昌街/延平南路東南側站：</strong>2/25 至 3/15，平日 17:00-22:00，假日 14:00-22:00 停止租還。<br>
+                        2. <strong>捷運西門3號出口(中華路一段/寶慶路)站：</strong>2/10 至 3/22，00:00-24:00 全天停止租還。
+                    </li>
+                    <li>
+                        <strong>自行車行駛管制：</strong><br>
+                        為避免自行車與行人交織發生危險，燈節期間：<br>
+                        - <strong>西門展區：</strong>中山堂廣場及西門紅樓廣場全天禁止進入；中華路(開封至長沙路段) 16:00-22:00 請下車牽行。<br>
+                        - <strong>花博展區：</strong>玉門至中山路段 16:00-22:00 請下車牽行。
+                    </li>
+                    <li>
+                        <strong>停車架取消：</strong><br>
+                        取消捷運西門站 1 號及 6 號出口周邊自行車停車架。
+                    </li>
+                </ul>
+
+                <h3 class="traffic-control-title" style="margin-top: 40px;">停車管制</h3>
+                <ul class="traffic-list">
+                    <li>
+                        <strong>禁止停車：</strong><br>
+                        燈節期間中華路...（請依據實際狀況補充完整路段）。
+                    </li>
+                </ul>
+            </div>
+
+            <div class="traffic-map-container">
+                <h3 style="color: white; margin-bottom: 20px;">交通管制示意圖</h3>
+                <img src="images/links/traffic_control_map.jpg" alt="2026台北燈節交通管制示意圖" class="traffic-map-img">
+            </div>
+
+        </div>
 
     </main>
 
